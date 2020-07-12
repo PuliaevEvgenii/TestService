092a1e0b13d6eb3a04fd3dfa337a78f53983abcc
Evgenii Puliaev <puliaevevgenii@gmail.com>
2020-07-12 18:27:27 +0300

    now login is unique for all users (checked during registration)

--- core/handle_registration.php
+++ core/handle_registration.php
@@ -9,37 +9,46 @@ $firstName = $_POST['firstName'];
 $middleName = $_POST['middleName'];
 $lastName = $_POST['lastName'];
 
-if ($password === $confirmPassword) {
-    $password = md5($password);
+$checkLogin = mysqli_num_rows(mysqli_query(
+    $connection,
+    "SELECT * FROM `user` WHERE `login` = '$login'"
+));
 
-    mysqli_query(
-        $connection,
-        "INSERT INTO `user` (`userID`, `firstName`, `middleName`, `lastName`, `login`, `password`, `role`) 
+if ($checkLogin != 0) {
+    $_SESSION['message'] = 'Этот логин уже занят';
+} else {
+    if ($password === $confirmPassword) {
+        $password = md5($password);
+
+        mysqli_query(
+            $connection,
+            "INSERT INTO `user` (`userID`, `firstName`, `middleName`, `lastName`, `login`, `password`, `role`) 
             VALUES (NULL, '$firstName', '$middleName', '$lastName', '$login', '$password', '1')"
-    );
+        );
 
-    $user = mysqli_fetch_assoc(mysqli_query(
-        $connection,
-        "SELECT * FROM `user` WHERE `login` = '$login' AND `password` = '$password'"
-    ));
+        $user = mysqli_fetch_assoc(mysqli_query(
+            $connection,
+            "SELECT * FROM `user` WHERE `login` = '$login' AND `password` = '$password'"
+        ));
 
-    if ($user) {
-        $_SESSION['message'] = 'Регистрация прошла успешно';
-        $_SESSION['user'] = [
-            'id' => $user['userID'],
-            'firstName' => $user['firstName'],
-            'middleName' => $user['middleName'],
-            'lastName' => $user['lastName'],
-            'login' => $user['login'],
-            'role' => $user['role']
-        ];
-        header('Location: ../index.php');
+        if ($user) {
+            $_SESSION['message'] = 'Регистрация прошла успешно';
+            $_SESSION['user'] = [
+                'id' => $user['userID'],
+                'firstName' => $user['firstName'],
+                'middleName' => $user['middleName'],
+                'lastName' => $user['lastName'],
+                'login' => $user['login'],
+                'role' => $user['role']
+            ];
+            header('Location: ../index.php');
+        } else {
+            $_SESSION['message'] = 'Что то пошло не так...';
+            header('Location: ../pages/registration.php');
+        }
+
     } else {
-        $_SESSION['message'] = 'Что то пошло не так...';
+        $_SESSION['message'] = 'Пароли не совпадают';
         header('Location: ../pages/registration.php');
     }
-
-} else {
-    $_SESSION['message'] = 'Пароли не совпадают';
-    header('Location: ../pages/registration.php');
 }
